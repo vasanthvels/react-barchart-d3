c9db24df18881330aec2e533774ccaffaf1d580c
vasanthvels <vasanthkumarvel@gmail.com>
2020-04-27 19:04:13 +0530

    Added bootstrap dependencies

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import'bootstrap/dist/css/bootstrap.min.css';
+import $ from'jquery';
+import Popper from'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
